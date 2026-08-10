09c8511ccbcc358691c61a59de89cb430689ef8f
makotha <kashanjamshaidawan@gmail.com>
2026-08-10 02:39:32 +0100

    Export v5.2 backend modules

---
+++ src/server/intake-v52/index.ts
@@ -0,0 +1,6 @@
+export * from "./contracts.ts";
+export * from "./date.ts";
+export * from "./routing.ts";
+export * from "./semantics.ts";
+export * from "./service.ts";
+export * from "./validation.ts";
